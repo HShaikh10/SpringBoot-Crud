b5ca2b5be925201c0ce2f292323e81768f5314d3
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-20 16:53:38 +0530

    Spring Boot Crud Repository

---
+++ src/com/springbootcrud/repository/SpringBootCrudRepository.java
@@ -0,0 +1,9 @@
+package com.springbootcrud.repository;
+
+public class SpringBootCrudRepository {
+
+	
+	public static void main(String[] args) {
+		System.out.println("SpringBoot-Crud Repository");
+	}
+}
